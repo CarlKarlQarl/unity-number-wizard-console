d9941388462e4ca7ba6fbb8c12ef44a21ef031ba
CarlKarlQarl <karlmatthes@gmail.com>
2020-07-23 16:57:43 -0600

    Set-up Update to listen for player input on each frame.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -9,14 +9,24 @@ public class NumberWizard : MonoBehaviour {
         int max = 1000;
 
 
-        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number in the following range.");
         Debug.Log("The lowest number is " + min);
         Debug.Log("The highest number is " + max);
+        Debug.Log("Tell if your number is lower or higher than 500.");
+        Debug.Log("Push Down = Lower, Push Up = Higher, Enter = Correct");
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down Arrow was pressed.");
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Debug.Log("Up Arrow was pressed.");
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Enter was pressed.");
+        }
     }
 }
