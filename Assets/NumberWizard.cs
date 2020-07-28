111952ecfd092053a447df292f826690551eb638
CarlKarlQarl <karlmatthes@gmail.com>
2020-07-27 19:41:36 -0600

    Refactored code out of Start and Update to reduce repetition, set-up game to restart once the game ends.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -10,6 +10,31 @@ public class NumberWizard : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            max = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow)){
+            min = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("I win!");
+            StartGame();
+        }
+    }
+
+    void StartGame() {
+
+        min = 1;
+        max = 1000;
+        guess = 500;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number in the following range.");
         Debug.Log("The lowest number is " + min);
@@ -19,20 +44,8 @@ public class NumberWizard : MonoBehaviour {
         max = max + 1;
     }
 
-    // Update is called once per frame
-    void Update() {
-        if(Input.GetKeyDown(KeyCode.DownArrow)){
-            max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Tell me if your number is lower or higher than " + guess);
-        }
-        else if(Input.GetKeyDown(KeyCode.UpArrow)){
-            min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Tell me if your number is lower or higher than " + guess);
-        }
-        else if(Input.GetKeyDown(KeyCode.Return)){
-            Debug.Log("I win!");
-        }
+    void NextGuess() {
+        guess = (max + min) / 2;
+        Debug.Log("Tell me if your number is lower or higher than " + guess);
     }
 }
